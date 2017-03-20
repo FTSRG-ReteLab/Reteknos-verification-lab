e127f36df7282bad67390f3bab2d99bfef3907b9
meres <rusz.marko@gmail.com>
2017-03-20 09:14:39 -0700

    test stubs added

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,17 +3,49 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.interfaces.TrainController;
+
 import static org.mockito.Mockito.*;
 
-public class TrainSensorTest {
+import javax.rmi.CORBA.Stub;
 
+public class TrainSensorTest {
+	TrainSensorImpl trainSensor;
+	
+	
+	
     @Before
     public void before() {
-        // TODO Add initializations
+    	TrainUser trainUser= mock(TrainUser.class);
+    	TrainController controller = mock(TrainController.class);
+        trainSensor = new TrainSensorImpl(controller,trainUser);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void NegativeSpeedLimitTest() {
+        
     }
+    
+    @Test
+    public void ValidSpeedLimitTest() {
+        
+    }
+    
+    @Test
+    public void TooLargeSpeedLimitTest() {
+        
+    }
+    
+    @Test
+    public void HalfSpeedLimitTest() {
+        
+    }
+    
+    @Test
+    public void MoreThanHalfSpeedLimitTest() {
+        
+    }
+    
 }
