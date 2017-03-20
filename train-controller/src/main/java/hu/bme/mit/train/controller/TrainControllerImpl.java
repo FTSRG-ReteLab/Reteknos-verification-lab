7bb1157772156d7f138fa627c69e77ad166614e1
haripet <harifaka@gmail.com>
2017-03-20 07:16:23 -0700

    The periodically FollowSpeed calling implemented with a timer task in the controller module

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,5 +1,8 @@
 package hu.bme.mit.train.controller;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import hu.bme.mit.train.interfaces.TrainController;
 
 public class TrainControllerImpl implements TrainController {
@@ -7,6 +10,8 @@ public class TrainControllerImpl implements TrainController {
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	private TimerTask tt=null;
+	private Timer t=new Timer();
 
 	@Override
 	public void followSpeed() {
@@ -40,10 +45,23 @@ public class TrainControllerImpl implements TrainController {
 			referenceSpeed = speedLimit;
 		}
 	}
+	
+	class SpeedSetTask extends TimerTask {
+        public void run() {
+           followSpeed();
+        }
+    }
 
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
-		this.step = joystickPosition;		
+		this.step = joystickPosition;	
+		if (this.step==0) {
+			if(tt!=null) tt.cancel();
+			if(tt!=null) tt=null;
+			return;
+		}
+		if(tt==null) tt= new SpeedSetTask();
+		t.schedule(tt, 1000,1000);
 	}
 
 }
